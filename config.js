9a1914f42be2c7139affcc5fcbaacebfe7668372
ankith sadashiva <ankith8@gmail.com>
2016-03-20 19:23:33 +0530

    initial commit
    removed node modules and initialized gitignore

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+"database" : "mongodb://root:abc123@ds019829.mlab.com:19829/myuserstory",
+"port": process.env.PORT || 3000,
+"secretKey": "YourSecretKey"
+
+}
---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var config = require('./config');
+var mongoose = require('mongoose');
+
+var app = express();
+mongoose.connect(config.database,function(err){
+  if(err){
+    console.log(err);
+  }else {
+    console.log("connected to database");
+  }
+});
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+var api = require('./app/routes/api')(app,express);
+app.use('/api',api);
+
+app.get("*",function(req,res){
+  res.sendFile(__dirname + '/public/views/index.html');
+});
+
+app.listen(config.port,function(err){
+  if(err){
+    console.log(err);
+  }else {
+    console.log("listening to port 3000");
+  }
+});
